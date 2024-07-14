96c6276a735079886a7da9e5f4df5f3f94acd266
Greg Izatt <gregory.izatt@gmail.com>
2024-07-14 02:32:19 -0400

    Lines demo that seems to work

--- calligraphy_printer/spiral_demo.py
+++ calligraphy_printer/spiral_demo.py
@@ -10,18 +10,7 @@ LOG.setLevel(logging.INFO)
 
 from grbl_gcode_streamer import GRBLGCodeStreamer, get_servo
 
-if __name__ == "__main__":
-    logging.basicConfig()
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("port", type=str)
-    args = parser.parse_args()
-
-    interface = GRBLGCodeStreamer(args.port, verbose=True)
-    interface.send_command("G90")
-    interface.send_command("G92 X0 Y0")
-    interface.send_command("M3")
-
+def run_spiral(interface):
     dt = 0.05
     ts = np.arange(0.0, 8.0 * np.pi + 0.01, dt)
     x = np.sin(ts / 2.0) * 50.0
@@ -48,3 +37,58 @@ if __name__ == "__main__":
 
     print("Done")
     plt.show()
+
+def wait_and_update(interface, delay: float):
+    start_time = time.time()
+    while time.time() - start_time < delay:
+        interface.update()
+        time.sleep(0.1)
+
+def wait_until_done(interface):
+    while len(interface.sent_command_buffer) > 0 or len(interface.command_queue) > 0:
+        interface.update()
+        time.sleep(0.1)
+
+def run_stroke_lines(interface):
+    interface.send_command(f"G1 X0 Y0 F3000 S{get_servo(0)}")
+    wait_until_done(interface)
+    
+    for line in range(5):
+        x = line * 10
+        interface.send_command(f"G1 X{x} Y0 S{get_servo(0)} F6000")
+        wait_until_done(interface)
+        
+        stroke_len = 100.
+        for t in np.linspace(0., 1., 20):
+            y = stroke_len * t
+            if t > 0.75:
+                stroke_depth = (1 - t)*4
+            elif t < 0.25:
+                stroke_depth = t*4
+            else:
+                stroke_depth = 1.0
+            stroke_depth = np.clip(stroke_depth, 0., 1.)
+            stroke_depth = 0.5 + (line + 1) * 0.1 * stroke_depth
+            interface.send_command(f"G1 X{x} Y{y} S{get_servo(stroke_depth)} F5000")
+        
+        interface.send_command(f"G1 X{x} Y{y} S{get_servo(0)} F6000")
+        wait_until_done(interface)
+
+    interface.send_command(f"G1 X0 Y0 S0 F6000")
+    wait_until_done(interface)
+
+if __name__ == "__main__":
+    logging.basicConfig()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", type=str)
+    args = parser.parse_args()
+
+    interface = GRBLGCodeStreamer(args.port, verbose=True)
+    interface.send_setting("$32=1")
+    interface.send_command("G90")
+    interface.send_command("G92 X0 Y0")
+    interface.send_command("M3")
+
+    #run_spiral(interface)
+    run_stroke_lines(interface)
